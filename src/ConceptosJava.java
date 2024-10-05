11c7e3175c4d6e5a611cc16da81daae91a44ea25
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2024-10-05 15:21:07 -0500

    Inclusión de operadores logicos.

--- src/ConceptosJava.java
+++ src/ConceptosJava.java
@@ -5,22 +5,19 @@ public class ConceptosJava {
         double doble = entero; // Conversion automatica (implicita)
         System.out.println("Conversion implicita de int a double:");
         System.out.println("Entero: " + entero+ " -> Doble: " + doble);
-        System.out.println();
 
         double numeroDoble = 9.89;
         int numeroEntero = (int) numeroDoble; // Conversion explicita
-        System.out.println("Conversion explicita de double a int:");
+        System.out.println("\nConversion explicita de double a int:");
         System.out.println("Doble: " + numeroDoble + " -> Entero: " + numeroEntero);
-        System.out.println();
 
         int x = 123;
         double y = 456.789;
         String cadena1 = Integer.toString(x); // Conversion a cadena
         String cadena2 = Double.toString(y);
-        System.out.println("Conversion de numeros a cadenas: ");
+        System.out.println("\nConversion de numeros a cadenas: ");
         System.out.println("Entero: " + x + " -> Cadena: \"" + cadena1 + "\"");
         System.out.println("Doble: " + y + " -> Cadena: \"" + cadena2 + "\"");
-        System.out.println();
 
         String cadenaEntero = "789";
         String cadenaDouble = "1064.1213";
@@ -28,7 +25,7 @@ public class ConceptosJava {
         try {
             int convertidoEntero = Integer.parseInt(cadenaEntero);
             double convertidoDouble = Double.parseDouble(cadenaDouble);
-            System.out.println("Conversion cadenas: ");
+            System.out.println("\nConversion cadenas: ");
             System.out.println("Cadena: \"" + cadenaEntero + "\" > Entero " + convertidoEntero);
             System.out.println("Cadena: \"" + cadenaDouble + "\" > Doble " + convertidoDouble);
         } catch (NumberFormatException e) {
@@ -67,5 +64,18 @@ public class ConceptosJava {
         System.out.println(m + " < " + n + " : " + (m < n));
         System.out.println(m + " >= " + n + " : " + (m >= n));
         System.out.println(m + " <= " + n + " : " + (m <= n));
+
+        // Operadores Logicos
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println("\nOperadores Logicos");
+        System.out.println(bool1 + " && " + bool2 + " : " + (bool1 && bool2));
+        System.out.println(bool1 + " || " + bool2 + " : " + (bool1 || bool2));
+        System.out.println("!" + bool1 + " : " + (!bool1));
+        System.out.println("!" + bool2 + " : " + (!bool2));
+
+
+
     }
 }
